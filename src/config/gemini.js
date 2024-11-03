e96bfb39ec7d89141ca34b54148083ade512b991
zelabbas <zakariaelabbassi2002@gmail.com>
2024-11-03 19:28:19 +0100

    ADD the all files

---
+++ src/config/gemini.js
@@ -0,0 +1,40 @@
+// const apikey = "";
+// import dotenv from "dotenv";
+// require("dotenv").config();
+
+// dotenv.config(); 
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  // const apiKey = process.env.GEMINI_API_KEY;
+  const apiKey = "AIzaSyBro5O6I9GqfyClgwVePgXD92Sb3j-qOXg";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompte) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompte);
+    console.log(result.response.text());
+    return (result.response.text());
+  }
+  
+export default  run;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ContextProvider from './context/context.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ContextProvider>
     <App />
-  </StrictMode>,
+  </ContextProvider>,
 )
