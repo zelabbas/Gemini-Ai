cd983a5c0b9ca0365c8fd68f90ab2917285292b4
zelabbas <zakariaelabbassi2002@gmail.com>
2024-11-11 09:59:54 +0100

    a small update to fix a samll error

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,39 +1,38 @@
 import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } from "@google/generative-ai";
-  
-  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompte) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run(prompte) {
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    try {
-      const result = await chatSession.sendMessage(prompte);
-      console.log(result.response.text());
-      return (result.response.text());
-    }catch {
-      console.error("Error generating response:", error);
-      return "An error occurred while generating the response.";
-    }
+
+  try {
+    const result = await chatSession.sendMessage(prompte);
+    console.log(result.response.text());
+    return result.response.text();
+  } catch (error) {
+    console.error("Error generating response:", error);
+    return "An error occurred while generating the response.";
   }
-  
-export default  run;
+}
+
+export default run;
